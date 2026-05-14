6446cc74408274982cade3a7bc83344e418ffb85
johnbayona <117481360+johnbayona@users.noreply.github.com>
2026-05-13 19:48:52 -0500

    feat: :sparkles: realizacion ejercicio 6

--- Ejercicio_5.py
+++ Ejercicio_5.py
@@ -1,2 +1,15 @@
 
 print("Bienvenidos al Programa de Calcular Propinas")
+
+cuenta = float(input("Ingresa el Valor Total de la Cuenta: "))
+porcentaje = float(input("Porcentaje de Propina (10, 15, 20): "))
+
+monto_propina = cuenta * (porcentaje / 100)
+
+total_pagar = cuenta + monto_propina
+
+print(f"Desglose Completo")
+print(f"Cuenta: ${cuenta:.2f}")
+print(f"Propina ({porcentaje}%): ${monto_propina:.2f}")
+print(f"Total a pagar: ${total_pagar:.2f}")
+print("Gracias por Usar el Programa de Calcular Propinas.")
